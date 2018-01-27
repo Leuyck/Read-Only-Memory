df185bb66dab6292c4f4b75aaf5a19fd87b57be7
Leuyck <loic.gillon@gmail.Com>
2018-01-27 18:35:42 +0100

    Ajout bouton pour main menu et son

---
+++ Read Only Memory/Assets/Scripts/PlayButtonAction.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PlayButtonAction : MonoBehaviour {
+    
+
+   /* private void OnMouseDown() {
+        ChangeLevel();
+    }*/
+
+    public void PlayTheGame() {
+
+        SceneManager.LoadScene("Scene 1");
+
+    }
+}
